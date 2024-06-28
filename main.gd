b14f2315444ac034775993c7d8a1d1100a099486
LucasGewl <lucasgermaincorriveau@gmail.com>
2024-06-28 18:57:45 -0400

    nothig

--- main.gd
+++ main.gd
@@ -2,6 +2,7 @@ extends Node
 
 var player
 var peer
+var ip_address
 
 const PORT = 8000
 const ADDRESS = "127.0.0.1"
@@ -9,17 +10,24 @@ const ADDRESS = "127.0.0.1"
 func _ready():
 	peer = ENetMultiplayerPeer.new()
 	player = load("res://player.tscn")
-	var network = ENetMultiplayerPeer.new()
-	network.create_server(8000)
-	multiplayer.multiplayer_peer = network
-	
-	network.peer_connected.connect(func(id): print("Player connected. ID: ", id))
 	
 	$player1.facing = 'right'
 	$player2.facing = 'left'
 	
 	$player1.who = '1'
 	$player2.who = '2'
+	
+	if OS.has_feature("windows"):
+		if OS.has_environment("COMPUTERNAME"):
+			ip_address =  IP.resolve_hostname(str(OS.get_environment("COMPUTERNAME")),1)
+		elif OS.has_feature("x11"):
+			if OS.has_environment("HOSTNAME"):
+				ip_address =  IP.resolve_hostname(str(OS.get_environment("HOSTNAME")),1)
+		elif OS.has_feature("OSX"):
+			if OS.has_environment("HOSTNAME"):
+				ip_address =  IP.resolve_hostname(str(OS.get_environment("HOSTNAME")),1)
+				
+	print(ip_address)
 
 # Called when the node enters the scene tree for the first time.
 func _on_host_pressed():
@@ -27,6 +35,7 @@ func _on_host_pressed():
 	multiplayer.multiplayer_peer = peer
 	peer.peer_connected.connect(func(id): print("peer is connected"))
 	print("ok")
+	print(IP.get_local_addresses())
 	
 
 func _on_join_pressed():
