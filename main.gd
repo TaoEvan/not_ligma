bdb6a2d5759542ea60352c9babb954fba810f76f
TaoEvan <tao.evan.pi@gmail.com>
2024-06-28 18:11:04 -0400

    network

--- main.gd
+++ main.gd
@@ -1,11 +1,27 @@
 extends Node
 
 var player
+var peer
 
-# Called when the node enters the scene tree for the first time.
+const PORT = 8000
+const ADDRESS = "127.0.0.1"
+
 func _ready():
+	peer = ENetMultiplayerPeer.new()
 	player = load("res://player.tscn")
 
+# Called when the node enters the scene tree for the first time.
+func _on_host_pressed():
+	peer.create_server(PORT)
+	multiplayer.multiplayer_peer = peer
+	peer.peer_connected.connect(func(id): print("peer is connected"))
+	print("ok")
+	
+
+func _on_join_pressed():
+	peer.create_client(ADDRESS, PORT)
+	multiplayer.multiplayer_peer = peer
+	print("ok")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
